e19f5fa28efbe9f290b1c4cac07465fbf20bb00e
adilson-strider <adilsonjunior.dev01@gmail.com>
2022-12-22 17:44:53 -0300

    game loop

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -1,10 +1,43 @@
 package main;
 
+import java.awt.Dimension;
+import java.awt.Color;
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     final int originalTileSize = 16;
-    final int scale = 2;
+    final int scale = 3;
     
+    final int tileSize = originalTileSize * scale; // 48 x 48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+
+    final int screenWidth = tileSize * maxScreenCol; // 768 px
+    final int screenHeight = tileSize * maxScreenRow; // 576 px
+
+    Thread gameThread;
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.BLACK);
+        this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread(){
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        while(gameThread != null){
+
+            System.out.println("Rodando...");
+        }
+
+    }
+
 }
